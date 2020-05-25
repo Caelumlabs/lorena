37a0a69809223e633bf3a7672254a8556b721228
Alex Puig <alex@caelumlabs.com>
2020-05-25 16:34:05 +0200

    FIrst commit

---
+++ packages/crypto/src/index.js
@@ -0,0 +1,138 @@
+'use strict'
+
+const { mnemonicGenerate, mnemonicToSeed, naclDecrypt, naclEncrypt } = require('@polkadot/util-crypto')
+const { stringToU8a, u8aConcat, u8aToHex, hexToU8a, hexToString } = require('@polkadot/util')
+const { randomAsU8a, naclKeypairFromSeed, naclSign, naclVerify, blake2AsHex } = require('@polkadot/util-crypto')
+
+/**
+ * Javascript Class to interact with Zenroom.
+ */
+module.exports = class LorenaCrypto {
+  /**
+   * Creates a new keypair.
+   *
+   * @returns {Promise} Return a promise with the execution of the creation.
+   */
+  newKeyPair () {
+    const mnemonic = mnemonicGenerate()
+    const keyPair = naclKeypairFromSeed(mnemonicToSeed(mnemonic))
+    return ({ mnemonic, keyPair })
+  }
+
+  /**
+   * @param {string} mnemonic Keypair from Seed.
+   *
+   * @returns {Promise} Return a promise with the execution of the creation.
+   */
+  keyPairFromSeed (mnemonic) {
+    const keyPair = naclKeypairFromSeed(mnemonicToSeed(mnemonic))
+    return ({ mnemonic, keyPair })
+  }
+
+  /**
+   * Creates a new keypair.
+   *
+   * @param {string} name  Holder of the keypair.
+   * @param {*} keys to create
+   * @returns {Promise} Return a promise with the execution of the creation.
+   */
+  async publicKey (name, keys) {
+    return new Promise((resolve) => {
+      resolve(true)
+    })
+  }
+
+  /**
+   * Encrypts (symmetric) a message with a keypair.
+   *
+   * @param {string} password Password to encrypt the message
+   * @param {string} message Message to be encrypted
+   * @returns {Promise} Return a promise with the execution of the encryption.
+   */
+  async encryptSymmetric (password, message) {
+    return new Promise((resolve) => {
+      let secret = stringToU8a(password)
+      secret = u8aConcat(secret, new Uint8Array(32 - secret.length))
+      const messagePreEncryption = stringToU8a(message)
+      const noncePreEncryption = randomAsU8a(24)
+
+      // Encrypt the message
+      const result = naclEncrypt(messagePreEncryption, secret, noncePreEncryption)
+
+      // Show contents of the encrypted message
+      resolve(result)
+    })
+  }
+
+  /**
+   * Encrypts (symmetric) a message with a keypair.
+   *
+   * @param {string} password Password to decrypt the message
+   * @param {string} msgEncrypted Message to be decrypted
+   * @returns {Promise} Return a promise with the execution of the encryption.
+   */
+  async decryptSymmetric (password, msgEncrypted) {
+    return new Promise((resolve) => {
+      let secret = stringToU8a(password)
+      secret = u8aConcat(secret, new Uint8Array(32 - secret.length))
+      const messageDecrypted = naclDecrypt(msgEncrypted.encrypted, msgEncrypted.nonce, secret)
+      resolve(hexToString(u8aToHex(messageDecrypted)))
+    })
+  }
+
+  /**
+   * Signs a message with a keypair.
+   *
+   * @param {string} message Message to be signed
+   * @param {object} keyPair Keypair for the signer
+   * @returns {object} Signature
+   */
+  signMessage (message, keyPair) {
+    const messageSignature = naclSign(message, keyPair)
+    return (u8aToHex(messageSignature))
+  }
+
+  /**
+   * Checks signature of a message.
+   *
+   * @param {string} message Message signed..
+   * @param {object} signature Signature of the message.
+   * @param {string} publicKey Public Key of the signature
+   * @returns {boolean} Wheter the signature is valid or not
+   */
+  checkSignature (message, signature, publicKey) {
+    return naclVerify(message, hexToU8a(signature), publicKey)
+  }
+
+  /**
+   * Create a Random string
+   *
+   * @param {number} length Length of the random string
+   * @returns {string} Return arandom string
+   */
+  random (length = 32) {
+    const rnd = hexToString(u8aToHex(randomAsU8a(length * 2)))
+    return (rnd.slice(0, length))
+  }
+
+  /**
+   * Creates a random Pin
+   *
+   * @param {number} length Length of the random PIN
+   * @returns {number} Random PIN
+   */
+  randomPin (length = 6) {
+    const rnd = randomAsU8a(length)
+    return (rnd.slice(0, length))
+  }
+
+  /**
+   * Create a Hash
+   *
+   * @param {string} source to be hashed
+   * @returns {string} Hashed source
+   */
+  blake2 (source) {
+    return (blake2AsHex(source))
+  }
+}
---
+++ packages/crypto/test/crypto.js
@@ -0,0 +1,110 @@
+const LorenaCrypto = require('../src/index')
+const chai = require('chai')
+  .use(require('chai-as-promised'))
+const assert = chai.assert
+
+const message = 'Hello World'
+const password = 'password random'
+
+let alice
+let signature = false
+let msgEncrypted = false
+let rnd = false
+const crypto = new LorenaCrypto(true)
+
+describe('LorenaCrypto', function () {
+  // Keypairs.
+  describe('1. KeyPair generation: ', () => {
+    it('Should create a new KeyPair', () => {
+      alice = crypto.newKeyPair()
+      assert.isNotEmpty(alice.mnemonic)
+      const mnemArray = alice.mnemonic.split(' ')
+      assert.equal(mnemArray.length, 12)
+      assert.isNotEmpty(alice.keyPair.publicKey)
+      assert.equal(alice.keyPair.publicKey.length, 32)
+      assert.isNotEmpty(alice.keyPair.secretKey)
+      assert.equal(alice.keyPair.secretKey.length, 64)
+    })
+
+    it('Should create a KeyPair from a Seed', () => {
+      const bob  = crypto.keyPairFromSeed(alice.mnemonic)
+      assert.equal(alice.keyPair.secretKey.toString, bob.keyPair.secretKey.toString)
+    })
+  })
+
+  describe('2. Hash Blake2: ', () => {
+    it('Should hash a String: ', () => {
+      const result = crypto.blake2('Hello world')
+      assert.isNotEmpty(result)
+    })
+  })
+
+  describe('3. Random: ', () => {
+    it('Should create a random String', () => {
+      rnd = crypto.random()
+      assert.isNotEmpty(rnd)
+      assert.equal(rnd.length, 32)
+      rnd = crypto.random(16)
+      assert.equal(rnd.length, 16)
+      rnd = crypto.random(8)
+      assert.equal(rnd.length, 8)
+    })
+
+    it('Should create a random PIN', () => {
+      rnd = crypto.randomPin()
+      assert.isNotEmpty(rnd)
+      assert.equal(rnd.length, 6)
+      rnd = crypto.randomPin(4)
+      assert.equal(rnd.length, 4)
+    })
+  })
+
+  describe('4. Signatures', () => {
+    it('Should create a new Signature: ', () => {
+      signature = crypto.signMessage(message, alice.keyPair)
+      assert.isNotEmpty(signature)
+    })
+
+    it('Should Check the Signature', () => {
+      const check = crypto.checkSignature(message, signature, alice.keyPair.publicKey)
+      assert.equal(check, true)
+    })
+  })
+
+  // Encryption.
+  describe('5. Encryption: ', () => {
+    it('Should encrypt (symmetric) a message', async () => {
+      msgEncrypted = await crypto.encryptSymmetric(password, message)
+      assert.isNotEmpty(msgEncrypted.encrypted)
+      assert.isNotEmpty(msgEncrypted.nonce)
+    })
+
+    it('Should decrypt (symmetric) a message', async () => {
+      const msg = await crypto.decryptSymmetric(password, msgEncrypted)
+      assert.equal(msg, message)
+    })
+  })
+  /*
+
+    it('Should NOT decrypt (symmetric) a message with wrong password: ', async () => {
+      const msg = await z.decryptSymmetric('badpassword', msgEncrypted)
+      assert.equal(msg.message, undefined)
+      assert.equal(msg.header, undefined)
+    })
+
+    it('Should encrypt (asymmetric) a message: ', async () => {
+      msgEncrypted = await z.encryptAsymmetric('Alice', aliceKeypair, 'Bob', bobPublic, message)
+      assert.isNotEmpty(msgEncrypted.secret_message)
+      assert.isNotEmpty(msgEncrypted.secret_message.iv)
+      assert.isNotEmpty(msgEncrypted.secret_message.header)
+      assert.isNotEmpty(msgEncrypted.secret_message.text)
+      assert.isNotEmpty(msgEncrypted.secret_message.checksum)
+    })
+
+    it('Should decrypt a  (asymmetric) message: ', async () => {
+      const msg = await z.decryptAsymmetric('Alice', alicePublic, 'Bob', bobKeypair, msgEncrypted)
+      assert.equal(msg.message, message)
+    })
+  })
+*/
+})
