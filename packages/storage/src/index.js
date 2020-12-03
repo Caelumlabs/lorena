470e1ee1f8111f25eb72d1a9b755509696ca8628
Alex Puig <apuig75@gmail.com>
2020-12-03 06:26:34 +0100

    Modified Storage to Fleek

--- packages/storage/src/index.js
+++ packages/storage/src/index.js
@@ -1,5 +1,6 @@
 'use strict'
-const IpfsClient = require('ipfs-http-client')
+// const IpfsClient = require('ipfs-http-client')
+const fleekStorage = require('@fleekhq/fleek-storage-js')
 
 /**
  * Javascript Class to interact with Zenroom.
@@ -8,20 +9,56 @@ module.exports = class Storage {
   /**
    * Constructor
    *
-   * @param {string} host connection information
+   * @param {string} apiKey Fleek API KEY
+   * @param {string} apiSecret Fleek API SECRET
    */
-  constructor (host = { host: 'localhost', port: 5001 }) {
-    this.params = host
-    this.ipfs = IpfsClient(host)
+  constructor (apiKey, apiSecret) {
+    this.apiKey = apiKey
+    this.apiSecret = apiSecret
   }
 
-  /**
-   * Puts data (DAG) to IPFS.
-   *
-   * @param {srting} name File name
-   * @param {object} data Data object
-   * @returns {Promise} CID or false
-   */
+  async add (name, data) {
+    const uploadedFile = await fleekStorage.upload({
+      apiKey: this.apiKey,
+      apiSecret: this.apiSecret,
+      key: name,
+      data: JSON.stringify(data)
+    })
+    return uploadedFile.hash
+  }
+
+  async get (cid) {
+    const uploadedFile = await fleekStorage.getFileFromHash({
+      hash: cid
+    })
+    return uploadedFile
+  }
+}
+
+/**
+ * Gets data (DAG) from IPFS.
+ *
+ * @param {string} cid ContentID
+ * @returns {Promise} data or false
+ */
+/*
+  async get (cid) {
+    let json = {}
+    const result = await this.ipfs.cat(cid)
+    for await (const item of result) {
+      json = JSON.parse(item.toString())
+    }
+    return json
+  }
+  */
+/**
+ * Puts data (DAG) to IPFS.
+ *
+ * @param {string} name File name
+ * @param {object} data Data object
+ * @returns {Promise} CID or false
+ */
+/*
   async add (name, data) {
     return new Promise((resolve) => {
       const str = JSON.stringify(data)
@@ -36,19 +73,4 @@ module.exports = class Storage {
         })
     })
   }
-
-  /**
-   * Gets data (DAG) from IPFS.
-   *
-   * @param {string} cid ContentID
-   * @returns {Promise} data or false
-   */
-  async get (cid) {
-    let json = {}
-    const result = await this.ipfs.cat(cid)
-    for await (const item of result) {
-      json = JSON.parse(item.toString())
-    }
-    return json
-  }
-}
+  */
--- packages/storage/src/index.test.js
+++ packages/storage/src/index.test.js
@@ -3,16 +3,25 @@ let storage, cid
 const didDoc = { id: 1, name: 'test' }
 
 test('Init Storage', async () => {
-  storage = new LorenaStorage()
+  storage = new LorenaStorage('Qs1xtyiXF8JGDTOO67HdWw==', 'iQIE4iYzWUfttLb6vfz2Qt8D/eC1prlq7SvdtBtuNo4=')
   expect(storage).toBeDefined()
 })
 
 test('Storage add a file to IPFS', async () => {
-  cid = await storage.add('did', didDoc)
+  cid = await storage.add('caelumlabs', didDoc)
   expect(cid).toBeDefined()
 })
 
-test('Storage get undefined: ', async () => {
+test('Storage get : ', async () => {
+  const result = await storage.get(cid)
+  expect(result).toBeDefined()
+  expect(result).toEqual(didDoc)
+})
+
+test('Add second Storage with different hash', async () => {
+  didDoc.id = 2
+  cid = await storage.add('dcs', didDoc)
+  expect(cid).toBeDefined()
   const result = await storage.get(cid)
   expect(result).toBeDefined()
   expect(result).toEqual(didDoc)
